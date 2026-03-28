62ac65ed618cf886b276b5f9b3691180d9a386c7
PATTIMANI MAHESH <mahibunny.34.s@gmail.com>
2026-03-28 11:02:29 +0530

    Update script.js

--- src/Frontend/script.js
+++ src/Frontend/script.js
@@ -154,7 +154,13 @@ function buildCharts(){
 
     <h2>📊 Topic-wise Sentiment</h2>
     <img src="data:image/png;base64,${globalData.topic_sentiment_chart}">
-
+    <h2>🚨 Top Negative Issues</h2>
+    <img src="data:image/png;base64,${globalData.negative_topics_chart}">
+    <h2>📊 Model Metrics</h2>
+    <img src="data:image/png;base64,${globalData.metrics_chart}">
+    
+    <h2>🧠 Confusion Matrix</h2>
+    <img src="data:image/png;base64,${globalData.confusion_matrix_chart}">
     <h2>🧠 AI Recommendations</h2>
     `;
 
